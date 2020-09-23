e3559492e1fa887abfe778b3db5da1ee57dbe931
Kumar Chandranshu <chandranshu@zestmoney.in>
2020-09-23 15:19:51 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,59 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo, deleteTodo, fetchTodos } from "../redux/actions/todos";
 
-export default function Home() {
+const TodoInput = () => {
+  const [todo, setTodo] = useState("");
+  const dispatch = useDispatch();
+  const addTodoHandler = () => {
+    dispatch(addTodo(todo));
+    setTodo("");
+  };
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      <input value={todo} onChange={(e) => setTodo(e.target.value)} />
+      <button onClick={addTodoHandler}>Add</button>
     </div>
-  )
-}
+  );
+};
+
+const Todos = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos.todos);
+  const deleteTodoHandler = (id) => {
+    dispatch(deleteTodo(id));
+  };
+  if (!todos.length) {
+    return <p>No Todos</p>;
+  }
+  return (
+    <ol>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.title}{" "}
+          <button
+            style={{ color: "red", marginBottom: "5px" }}
+            onClick={() => deleteTodoHandler(todo.id)}
+          >
+            Remove
+          </button>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, []);
+  return (
+    <>
+      <TodoInput />
+      <Todos />
+    </>
+  );
+};
+
+export default App;
